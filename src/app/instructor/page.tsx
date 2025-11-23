ca788eeb4c2d994eca477b4144756b6e0e7ef61e
raximovhayot <110677990+raximovhayot@users.noreply.github.com>
2025-11-23 10:18:17 +0000

    deleted

--- src/app/instructor/page.tsx
+++
@@ -1,7 +0,0 @@
-import { redirect } from 'next/navigation';
-
-import { ROUTES_APP } from '@/features/instructor/routes';
-
-export default function InstructorPage() {
-  redirect(ROUTES_APP.quizzes.list());
-}
